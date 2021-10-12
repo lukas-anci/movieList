52d8bb11de616174f122b3b64f583f9f610adbd6
Lukas <lukas.ancikevicius@gmail.com>
2021-10-12 15:15:05 +0300

    added fa, css, header, pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,29 @@
-import './App.css';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
+import './App.css';
+import Header from './components/Header';
+import Watchlist from './components/Watchlist';
+import Watched from './components/Watched';
+import Add from './components/Add';
+import './lib/font-awesome/css/all.min.css';
 function App() {
-  return <div className="App">start</div>;
+  return (
+    <Router>
+      <Header />
+
+      <Switch>
+        <Route exact path="/">
+          <Watchlist />
+        </Route>
+        <Route exact path="/watched">
+          <Watched />
+        </Route>
+        <Route exact path="/add">
+          <Add />
+        </Route>
+      </Switch>
+    </Router>
+  );
 }
 
 export default App;
